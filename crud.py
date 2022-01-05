886802384a429a802900ce8ae01359c3c5dccd83
Lisa Murray <lisamichellemurray@gmail.com>
2022-01-05 22:59:30 +0000

    finished part 3

--- crud.py
+++ crud.py
@@ -12,6 +12,15 @@ def create_user(email, password):
 
     return user
 
+def return_all_users():
+    """Return all users"""
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return user from user id"""
+
+    return User.query.get(user_id)
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -21,6 +30,15 @@ def create_movie(title, overview, release_date, poster_path):
     db.session.commit()
 
     return movie
+
+def return_all_movies():
+    """Return all movies"""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Return movie description based on movie id"""
+
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
     """Create and return a new rating"""
--- server.py
+++ server.py
@@ -1,14 +1,54 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import connect_to_db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = 'dev'
+app.jinja_env.undefined = StrictUndefined
 
 
 # Replace this with routes and view functions!
 
+@app.route('/')
+def homepage():
+    """ view homepage """
+
+    return render_template('homepage.html')
+
+@app.route('/movies')
+def show_all_movies():
+
+    list_movies = crud.return_all_movies()
+
+    return render_template('movies.html', list_movies=list_movies)
+
+@app.route('/movies/<movie_id>')
+def show_movie(movie_id):
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+@app.route('/users')
+def show_all_users():
+
+    list_users = crud.return_all_users()
+
+    return render_template('users.html', list_users=list_users)
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_details.html', user=user)
+
 
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
