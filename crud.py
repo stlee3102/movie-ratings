ae8b3533d4b1f622cf626fd848269ad0db0c3675
Lisa Murray <lisamichellemurray@gmail.com>
2022-01-06 00:44:15 +0000

    partway through part 4

--- crud.py
+++ crud.py
@@ -21,6 +21,11 @@ def get_user_by_id(user_id):
 
     return User.query.get(user_id)
 
+def get_user_by_email(email):
+    """Return user from email"""
+
+    return User.query.filter(User.email == email).first()
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
--- server.py
+++ server.py
@@ -33,12 +33,59 @@ def show_movie(movie_id):
 
     return render_template('movie_details.html', movie=movie)
 
+@app.route("/rate_movie/<movie_id>", methods=["POST"])
+def rate_movie(movie_id):
+
+    movie = crud.get_movie_by_id(movie_id)
+    score = request.form.get("score")
+    user = crud.get_user_by_id(session['user'])
+
+    new_rating = crud.create_rating(user, movie, score)
+    
+    movie_ratings = movie.ratings
+
+    return render_template('movie_details.html', movie_ratings = movie_ratings)
+
 @app.route('/users')
 def show_all_users():
 
     list_users = crud.return_all_users()
 
     return render_template('users.html', list_users=list_users)
+
+@app.route('/users', methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash("User already created")
+    else:
+        crud.create_user(email, password)
+        flash("Account registered")
+    
+    return redirect("/")
+
+
+@app.route('/login', methods=["POST"])
+def login_user():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if user:
+        if password == user.password:
+            session['user'] = user.user_id
+            flash(f"logged in {user.user_id} {user.email} {user.password}")
+        else:
+            flash("incorrect password, try again")
+    else:
+        flash("user not registered")
+
+    return redirect("/")
 
 @app.route('/users/<user_id>')
 def show_user(user_id):
